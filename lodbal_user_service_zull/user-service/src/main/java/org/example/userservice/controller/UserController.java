46cf20e6c77979e3af4b42eb1a7d9b505d4a8a31
lovikagupta <66506045+lovikagupta@users.noreply.github.com>
2020-06-07 22:36:32 +0530

    ZULL on discovery+User Service laod bal using ZULL

---
+++ lodbal_user_service_zull/user-service/src/main/java/org/example/userservice/controller/UserController.java
@@ -0,0 +1,34 @@
+package org.example.userservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users123")
+public class UserController {
+	private Environment env;
+	@Autowired
+    public UserController(Environment env) {
+		super();
+		this.env = env;
+	}
+
+	@GetMapping("/status/check")
+	/*public String check()
+	{
+		return "user service working ";
+	}*/
+
+    public String check()
+    {
+    	System.out.print("xxxxxxxxxxxxxxxx"+env.getProperty("local.server.port"));
+        return "user service working: "+env.getProperty("local.server.port");
+    }
+
+
+
+}
